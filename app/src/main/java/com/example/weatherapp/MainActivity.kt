399233036e579c31b671fbde72b306a85850c5f9
danilfs009@yandex.ru <62214743+danilfs@users.noreply.github.com>
2022-03-28 09:52:58 +0300

    Lesson 6 Without FragmentThreadBinding

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,20 +2,32 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.weatherapp.databinding.ActivityMainBinding
+import com.example.weatherapp.experiment.MainBroadCastReceiver
 import com.example.weatherapp.view.MainFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val receiver = MainBroadCastReceiver()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commit()
+            openFragment(MainFragment.newInstance())
+//            openFragment(ThreadsFragment.newInstance())
         }
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commit()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
